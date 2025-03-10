431e5be8cf654f9d3bc24fcd77dada59ab01b20c
root <153816913+rasrayn@users.noreply.github.com>
2025-03-10 14:22:50 +0100

    agrego style y cambios en el codigo

--- script.js
+++ script.js
@@ -1,79 +1,50 @@
-/**
-juego piedra papel tijera:
-primero creando una funcion para que la computa elija de forma aleatoria entre piedra papel tijera.
-segundo una funcion para que el jugador elija entre las tres opciones y ver quien gana
-tercero crear unas puntuaciones entre usuario y computadora
-el juego tendra 5 rondas y podra escribirse en mayusculas o minusculas.
-*/  
 
-//funcion para sumar los puntos
 
-function addHumanScore(){
-    humanScore++;
-}
-function addComputerScore(){
-    computerScore++;
-}
 let humanScore = 0;
 let computerScore = 0;
 
-// creación para los 5 rounds
+//funcion para sumar los puntos
 
-for (let round = 1; round <= 5; round++) {
+function playGame(userChoice){
+    const choice = ['piedra', 'papel', 'tijera'];
+    const computerChoice = choice[Math.floor(Math.random() * 3)]
+    
+/*forma mas sencilla de hacer lo de arriba
+const choice = ['piedra', 'papel', 'tijera'];
+const computerChoice = choice[Math.floor(Math.random() * 3)]
+*/
+    let result = ""
 
-    let humanChoice = prompt("Piedra, Papel o Tijera, 1, 2 y 3: ").toLowerCase();
-        console.log(humanChoice);
+    if (computerChoice === userChoice) {
+        result = `Draw. Both player choice ${userChoice}`;
 
-    function getComputerChoice() {
-        return Math.random()
+    }else if (
+        userChoice === "piedra" && computerChoice === "tijera" || userChoice === "papel" && computerChoice === "piedra" || userChoice === "tijera" && computerChoice === "papel"
+        
+    ){
+        result = `You Winner!. ${userChoice} win ${computerChoice}`
+        humanScore++
+
+    }else {
+        result = `You lose!. ${computerChoice} win ${userChoice}`
+        computerScore++
     }
-    let computerChoice = getComputerChoice();
+    document.getElementById("result").textContent = result;
+    document.getElementById("score").textContent = `JUGADOR: ${humanScore} | COMPUTADORA: ${computerScore}`
 
-// Conversión del resultado de Math.random en la elección de piedra papel tijera.
-    if (computerChoice <= 0.33) {
-        computerChoice= "Tijera"
-        console.log("Tijera")
-    } else if (computerChoice <= 0.66) {
-        computerChoice = "Piedra"
-        console.log("Piedra")
-    } else if (computerChoice <= 0.99) {
-        computerChoice = "Papel"
-        console.log("Papel")
+    if (humanScore == 3 || computerScore == 3) {
+        endGame()
     }
+}    
 
+function endGame () {
+    const finalResult = humanScore == 3 ? 'Enhorabuena, ¡has ganado!' : 'Lo siento, ¡Has perdido!'
+    document.getElementById('result').textContent = finalResult;
 
-    // Definicion de quien gana y sistema de puntuacion
-    if (computerChoice.toLowerCase() == humanChoice) {
-        console.log("Draw!")
-    } else if (computerChoice === "Tijera" && humanChoice === "piedra") {
-        addHumanScore();
-        console.log("Ganaste! Piedra gana a Tijeras.")
-    } else if (computerChoice === "Piedra" && humanChoice === "papel") {
-        addHumanScore();
-        console.log("Ganaste! Papel gana a Piedra.")
-    } else if (computerChoice === "Papel" && humanChoice === "tijera") {
-        addHumanScore();
-        console.log("Ganaste! Tijera gana a Papel.")
-    } else if (computerChoice === "Tijera" && humanChoice === "papel") {
-        addComputerScore();
-        console.log("Has perdido! Tijeras gana a Papel-")
-    } else if (computerChoice === "Piedra" && humanChoice === "tijera") {
-        addComputerScore();
-        console.log("Has perdido! Piedra gana a Tijera.")
-    } else if ( computerChoice === "Papel" && humanChoice === "piedra") {
-        addComputerScore();
-        console.log("Has perdido! Papel gana a Piedra.")
-    }
-
-    console.log(`Usuario: ${humanScore} Rival: ${computerScore}`);
-    console.log(`Round: ${round}`);
+    document.getElementById('btnTijera').disabled = true;
+    document.getElementById('btnPiedra').disabled = true;
+    document.getElementById('btnPapel').disabled = true;
 }
 
-if (humanScore == computerScore) {
-console.log("ESTO ES UN EMPATE!!")
-} else if (humanScore > computerScore) {
-    console.log("ENHORABUENA GANASTE!!!")
-} else if (humanScore < computerScore) {
-    console.log("TAL VEZ EN OTRO MOMENTO... HAS PERDIDO!")
-}
+
 
